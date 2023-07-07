78b16acec91d8c00725a01da8d44a9b18f5abab5
ChethanaSV-22 <chethanasv02@gmail.com>
2023-07-07 18:01:07 +0530

    final changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
 
         <div className=" flex justify-center items-center">
           <form
-            action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
+            action="https://getform.io/f/71c28741-ca54-4620-ba90-2cdd40a032a2"
             method="POST"
             className=" flex flex-col w-full md:w-1/2"
           >
